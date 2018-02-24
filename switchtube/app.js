536239dc029f2076c32765d6130468c3d3411ea6
Ep8Script <34702@stpats.vic.edu.au>
2018-02-24 12:10:46 +1100

    Add files via upload

--- switchtube/app.js
+++ switchtube/app.js
@@ -250,7 +250,7 @@ function myChannel() {
 function loadSubscriptions(e) {
 	$("#subscriptions").show();
 	$.ajax({
-		  url: "https://www.googleapis.com/youtube/v3/subscriptions?mine=true&access_token="+accessToken+"&part=snippet&maxResults=20", 
+		  url: "https://www.googleapis.com/youtube/v3/subscriptions?mine=true&access_token="+clientToken+"&part=snippet&maxResults=20", 
 		  dataType: "json",
 		  success: function(data) {
 			  subs = 0;
