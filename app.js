09259f0698bb13fd17b759f7bcbf8820766e8451
Ep8Script <34702@stpats.vic.edu.au>
2018-03-03 22:14:50 +1100

    Add files via upload

--- app.js
+++ app.js
@@ -456,7 +456,7 @@ function myChannel() {
 function loadSubscriptions(e) {
 	$("#subscriptions").show();
 	$.ajax({
-		  url: "https://www.googleapis.com/youtube/v3/subscriptions?mine=true&access_token="+clientToken+"&part=snippet&maxResults=20", 
+		  url: "https://www.googleapis.com/youtube/v3/subscriptions?mine=true&access_token="+clientToken+"&part=snippet&maxResults=50", 
 		  dataType: "json",
 		  success: function(data) {
 			  subs = 0;
