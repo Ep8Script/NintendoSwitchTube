84de4301eaad54714e1fcf5efdf9f95cce6ff872
Ep8Script <34702@stpats.vic.edu.au>
2018-09-23 18:05:58 +1000

    Stan Loona

--- app.js
+++ app.js
@@ -179,7 +179,7 @@ $(document).ready(function() {
 		var value = this.value;
 		if(value == "video") {
 			$("#vidID .get-video").attr("placeholder", "Enter YouTube Video ID");
-			$(".example").html('Example: <a href="#" onclick="getVideo(\'4KRdmXcJWQU\')">4KRdmXcJWQU</a>');
+			$(".example").html('Example: <a href="#" onclick="getVideo(\'gSRFqpheg6Y\')">gSRFqpheg6Y</a>');
 		}
 		else if(value == "channel") {
 			$("#vidID .get-video").attr("placeholder", "Enter YouTube Channel ID");
